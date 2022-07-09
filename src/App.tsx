cc2e214e71ec39f482eed361a93b08781ab4ee8f
Dastan Ozgeldi <ozgdastan@gmail.com>
2022-07-09 18:31:01 +0600

    feat(*): Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { IoHome, IoLogoGithub, IoLogoLinkedin, IoLogoTwitter } from "react-icons/io5";
 
 function App() {
+  const iconStyle = {width: 20, height: 20}
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <img src={require("./profile.jpg")} alt="profile" />
+        <h1>@dositan</h1>
       </header>
+      <ul>
+        <li>
+          <a
+            href="https://dositan.vercel.app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <IoHome style={iconStyle} /> Portfolio
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://github.com/Dositan"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <IoLogoGithub style={iconStyle} /> Github
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://twitter.com/dastanozgeldi"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <IoLogoTwitter style={iconStyle} /> Twitter
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://linkedin.com/in/dosek"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <IoLogoLinkedin style={iconStyle} /> Linkedin
+          </a>
+        </li>
+      </ul>
     </div>
   );
 }
